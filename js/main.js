175f753d1aefa6c4d9bcff32c3e4c5d209ace2d4
albertost5 <sancheztorresalberto@gmail.com>
2022-02-23 20:36:19 +0100

    Added new js with the error handling.

--- js/main.js
+++ js/main.js
@@ -1,44 +1,45 @@
-let loadBtn = document.getElementById("btn_load_users");
-let loaderDiv = document.getElementById("loader");
+let loadBtn = document.getElementById('btn_load_users');
+let loaderDiv = document.getElementById('loader');
+let errorBox = document.getElementById('error_box');
+let userTable = document.getElementById('user_table');
 
-
-
-loadBtn.addEventListener('click', function(){
+const loadUsers = () => {
+    userTable.innerHTML = '<tr><th>ID</th><th>Name</th><th>Age</th><th>Country</th><th>Email</th></tr>';
 
     let request = new XMLHttpRequest();
-    // request.open('GET', 'https://api.json-generator.com/templates/Kn4n6fEVUIsC/data');
     request.open('GET', 'php/users.php');
-    request.setRequestHeader('Authorization', 'Bearer ' + 'i3xud8ubg5ll5yypdv2wzumdz5zlxo88grhakmrf');
-    
     loaderDiv.classList.add('active');
 
     request.onload = function(){
         // Get data
         let data = JSON.parse(request.responseText);
-
-        // Print data
-        data.forEach(user => {
-            let row = document.createElement('tr');
-            row.innerHTML += ("<td>" + user.id + "</td>");
-            row.innerHTML += ("<td>" + user.name + "</td>");
-            row.innerHTML += ("<td>" + user.age + "</td>");
-            row.innerHTML += ("<td>" + user.country + "</td>");
-            row.innerHTML += ("<td>" + user.email + "</td>");
-            document.getElementById('user_table').appendChild(row);
-        });
-    };
-
-    // 2 request received
-    // 3 processing request
-    // 4 response ready
-
-    request.onreadystatechange = function(){
         
-        if(request.readyState == 4 && request.status == 200){
-            loaderDiv.classList.remove('active');
+        if(data.error){
+            errorBox.classList.add('active');
+        }else{ 
+            // Print data
+            data.forEach(user => {
+                let row = document.createElement('tr');
+                row.innerHTML += ('<td>' + user.id + '</td>');
+                row.innerHTML += ('<td>' + user.name + '</td>');
+                row.innerHTML += ('<td>' + user.age + '</td>');
+                row.innerHTML += ('<td>' + user.country + '</td>');
+                row.innerHTML += ('<td>' + user.email + '</td>');
+                document.getElementById('user_table').appendChild(row);
+            });
         }
 
     };
 
+    request.onreadystatechange = () => {     
+        if(request.readyState == 4 && request.status == 200){
+            loaderDiv.classList.remove('active');
+        }
+    };
+
     request.send();
+}
+
+loadBtn.addEventListener('click', function(){
+    loadUsers();
 });
